673fafaaa4a3a0b0101f82974210649b97c9f3e8
Ewan <ewan.borsa@samtrevano.ch>
2023-02-03 16:08:51 +0100

    auto push

--- 5_Applicativo/index.php
+++ 5_Applicativo/index.php
@@ -2,7 +2,9 @@
 <html>
 <head>
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" href="css/upload.css">
+    <link rel="stylesheet" href="css/index-upload.css">
+    <link href="css/dropzone.css" type="text/css" rel="stylesheet" />
+    <script src="dropzone.js"></script>
     <script>
         var i = 0;
         function move() {
@@ -26,17 +28,22 @@
 </head>
 <body style="text-align: center;">
     <h1 class="video-processing">Video Processing Cluster</h1>
+    
     <div class="rect-upload">
         <img src="images/upload.png" alt="UPLOAD" width="100" style="margin:10px"></img>
         <br><button class="button-upload">Carica il tuo video</button><br>
         <br><hr><div class="altrimenti">ALTRIMENTI</div><hr><br>
+        <form action="upload.php" class="dropzone">
         <div class="rilascia-qui">Trascina e rilascia qui</div><br>
         <div class="formati-file">Formati file supportati: MP4, MOV, MKV, AVI, WMV, FLV, GIF<br>Max. 500MB</div>
+        </form>
+        
     </div>
     <br><br>
     <div class="progress-bar" id="myProgress">
         <div class="progress-bar" id="myBar"></div>
     </div>
+    
     <br><br><br><br><br><br><br><br>
     <a href="https://www.flaticon.com/free-icons/video" title="video icons">Video icons created by Hilmy Abiyyu A. - Flaticon</a>
 </body> 
---
+++ 5_Applicativo/uploads/README.md
@@ -0,0 +1,9 @@
+# VideoProcessingCluster
+Progetto 2° Semestre SAM I3BB 2023
+
+Studenti:
+Ewan Borsa
+Alessandro Castelli
+Matteo Rüedi
+
+CPT Trevano
