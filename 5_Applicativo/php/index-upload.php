3a268e1a8789f1c21f415cafa2eb08edc0fb30c2
Ewan <ewan.borsa@samtrevano.ch>
2023-03-10 15:38:24 +0100

    autopush

--- 5_Applicativo/php/index-upload.php
+++
@@ -1,7 +0,0 @@
-<?php
-session_start();
-$_SESSION["session_id"] = random_int(100000000, 999999999);
-print_r("Id Sessione: " . $_SESSION["session_id"]);
-readfile("../html/upload.html");
-include "upload.php";
-?>
