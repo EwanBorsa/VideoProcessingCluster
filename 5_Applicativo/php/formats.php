27b0f836df74cd545ec7098940be8568754fa214
Ewan <ewan.borsa@samtrevano.ch>
2023-03-17 15:04:45 +0100

    autopush

--- 5_Applicativo/php/formats.php
+++ 5_Applicativo/php/formats.php
@@ -3,17 +3,26 @@
 <head>
 </head>
 <body>
-<h1>File formats</h1>
+<h1>Accepted file formats</h1>
 <pre>
-	<h2>Format Name</h2>	<h2>Format Extension</h2>
-	Audio Video Interleave 	avi
-	QuickTime movie file	mov 
-	MPEG-4					mp4
-	Flash video file		flv
-	Flash MP4 Video File	f4v
-	WebM video file			wepb
-	MPEG-4 Video			m4v
-	Shockwave Flash			swf
+<b>Name			Extension</b>
+------------------------------------------------
+Audio Video Interleave	.avi
+------------------------------------------------
+QuickTime movie file	.mov 
+------------------------------------------------
+MPEG-4			.mp4
+------------------------------------------------
+Flash video file	.flv
+------------------------------------------------
+Flash MP4 Video File	.f4v
+------------------------------------------------
+WebM video file		.wepb
+------------------------------------------------
+MPEG-4 Video		.m4v
+------------------------------------------------
+Shockwave Flash		.swf
+------------------------------------------------
 </pre>
 </body>
 </html>
