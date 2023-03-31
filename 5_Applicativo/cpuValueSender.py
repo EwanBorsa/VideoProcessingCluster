786846b6f80a3ce6c3d52f16fd19b2f94e81d923
Ewan <ewan.borsa@samtrevano.ch>
2023-03-31 16:15:32 +0200

    autopush

--- 5_Applicativo/cpuValueSender.py
+++ 5_Applicativo/cpuValueSender.py
@@ -1,36 +1,47 @@
-#!/usr/bin/python
+#!/usr/bin/python3
 
+import time
 import psutil
 import socket
-import sys
-import time
 
-# LoadBalancer IP
-IP = '192.168.56.30'
-# Define the port on which you want to connect
-PORT = 9999
+# HOST = '192.168.56.30'  # LoadBalancer IP
 
-# Create a socket object that send data
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    # Bind socket to localhost and port 9999
-    try:
-        s.bind(('', PORT))
-    except socket.error as msg:
-        print('\nBind failed. Error Code : ' + str(msg[0]) + ' Message ' + msg[1])
-        sys.exit()
-    print('\nSocket bind complete')
-    s.listen()
-    print('\nSocket now listening')
-    conn, addr = s.accept()
-    with conn:
-        print(f"\nConnected by {addr}")
-        while 1:
-            cpu = psutil.cpu_percent(interval=1, percpu=False)
-            print(f"\nCpu value sent: {cpu}%")
-            if cpu > 50:
-                # Set server weight to half
-                conn.sendall(bytes(ascii("50%\n")))
-            elif cpu < 10:
-                conn.sendall(bytes(ascii("100%\n")))
-            time.sleep(1)
-    conn.close()
+PORT = 9999  # Socket Port
+
+while True:
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    print("socket created")
+    if s:
+        try:
+            try:
+                s.bind(('', PORT))
+                print("Socket bound")
+            except socket.error as msg:
+                print('\nBind failed. Error Code : ' + str(msg[0]) + ' Message ' + msg[1])
+                exit()
+            s.listen()
+            print(f"Socket({PORT}) is listening")
+            with s.accept() as (conn, addr):
+                print(f"Connected by {addr}")
+                agent_request = conn.recv(1024).decode()
+                print(f'agent request: {agent_request}')
+                cpu = psutil.cpu_percent(interval=1, percpu=False)
+                print(f"Cpu: {cpu}%")
+                response = ""
+                if cpu > 10.0:
+                    # Set server weight to half
+                    response = "50%\n"
+                else:
+                    response = "100%\n"
+                    ascii_response = response.encode(encoding="ascii", errors="ignore")
+                conn.sendall(ascii_response)
+                print(f'data sent: {response}')
+            s.shutdown(socket.SHUT_RDWR)
+            print('socket down')
+            s.close()
+            print('socket closed')
+        except Exception as e:
+            print(e)
+    time.sleep(0.1)
+    print('.', end='')
+
---
+++ 5_Applicativo/progetto_vagrant_test/scripts/provision_memcached.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+echo "memcached provisioning - begin"
+
+apt-get install memcached
+
+#wget http://memcached.org/latest
+
+#tar -zxvf memcached-1.6.19.tar.gz
+
+#cd memcached-1.6.19
+
+#./configure && make && make test && sudo make install
+
+echo "memcached provisioning - end"
